a7875d668aab98a2e1fc1d3d03098babe9282e49
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2025-04-21 15:51:14 +0000

    continue and complete if else

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("from conditionals we are about to continue!")
+// multiple conditions!!
+// write a program to display grades according to your percentage
+
+/*
+
+(90 - 99) => A
+(80 - 89) => B
+(70 - 79) => C
+(60 - 69) => D
+
+
+*/
+
+let student_percentage = 68;
+
+if (student_percentage >= 90 && student_percentage <= 99) {
+    console.log("GRAGE => A")
+}
+else if (student_percentage >= 80 && student_percentage <= 89) {
+    console.log("GRAGE => B")
+}
+else if (student_percentage >= 70 && student_percentage <= 79) {
+    console.log("GRAGE => C")
+}
+else if (student_percentage >= 60 && student_percentage <= 69) {
+    console.log("GRAGE => D")
+}
