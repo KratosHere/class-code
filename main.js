1fb8d81e0157694f7e8f1ce9c969af4be56d5c8a
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2025-04-22 15:40:31 +0000

    complete and end conditionals in next lecture

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
-// multiple conditions!!
-// write a program to display grades according to your percentage
+const actualMail = "samyak123@gmail.com" // stored during registration
+const actualPassword = "sam321"; // stored during registration
 
-/*
+let enteredMail = "samyak123@gmail.com";
+let enteredPassword = "sam321";
 
-(90 - 99) => A
-(80 - 89) => B
-(70 - 79) => C
-(60 - 69) => D
-
-
-*/
-
-let student_percentage = 68;
-
-if (student_percentage >= 90 && student_percentage <= 99) {
-    console.log("GRAGE => A")
-}
-else if (student_percentage >= 80 && student_percentage <= 89) {
-    console.log("GRAGE => B")
-}
-else if (student_percentage >= 70 && student_percentage <= 79) {
-    console.log("GRAGE => C")
-}
-else if (student_percentage >= 60 && student_percentage <= 69) {
-    console.log("GRAGE => D")
+if ((enteredMail === actualMail) && (enteredPassword === actualPassword)) {
+    console.log("USER LOGGED IN!!")
+} else {
+    console.log("INVALID INFO!!");
 }
