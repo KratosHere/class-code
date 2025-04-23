b2f18c714d4019d2e2072e3c3176be0f782bab35
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2025-04-23 15:32:05 +0000

    continuing from loops from next lecture

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const actualMail = "samyak123@gmail.com" // stored during registration
+const actualMail = "samyak123@23gmail.com" // stored during registration
 const actualPassword = "sam321"; // stored during registration
+const actualPhonenumber = 11;
 
 let enteredMail = "samyak123@gmail.com";
+let enteredPhonenumber = 11;
 let enteredPassword = "sam321";
 
-if ((enteredMail === actualMail) && (enteredPassword === actualPassword)) {
-    console.log("USER LOGGED IN!!")
+
+if ((enteredMail === actualMail || enteredPhonenumber === actualPhonenumber) && (enteredPassword === actualPassword)) {
+    console.log("User logged in!!");
 } else {
-    console.log("INVALID INFO!!");
+    console.log("Invalid Credentials!")
 }
